e7137d9a80e4eb519fe63b0aa72843b6c3a5faf6
Hardeep <deshardeep@gmail.com>
2024-06-28 12:14:24 +0530

    Developer website With Animation

---
+++ script.js
@@ -0,0 +1,125 @@
+let tl = gsap.timeline();
+tl.to("#circlejs", {
+  duration: 1,
+  delay: 1,
+  scale: 1,
+  y: 120,
+});
+tl.to("#circlejs", {
+  x: -120,
+  y: 300,
+  duration: 1,
+  delay: 3,
+});
+tl.to(".javascript", {
+  duration: 1,
+  delay: 1.3,
+  scale: 1,
+  y: 85,
+});
+tl.to(".javascript", {
+  x: -120,
+  y: 290,
+  duration: 1,
+  delay: 1.5,
+});
+
+tl.to("#circlecplusplus", {
+    duration: 1,
+    delay: 1,
+    scale: 1,
+    y: 85,
+  });
+  tl.to("#circlecplusplus", {
+    x: -235,
+    y: 70,
+    duration: 1,
+    delay: 3,
+  });
+  tl.to(".Cplusplus", {
+    duration: 1,
+    delay: 1.3,
+    scale: 1,
+    y: 85,
+  });
+  tl.to(".Cplusplus", {
+    x: -235,
+    y: 65,
+    duration: 1,
+    delay: 1.5,
+  });
+
+tl.to("#circleAngular", {
+  duration: 1,
+  delay: 1,
+  scale: 1,
+  y: 85,
+});
+tl.to("#circleAngular", {
+  x: 140,
+  y: 150,
+  duration: 1,
+  delay: 3,
+});
+tl.to(".Angular", {
+  duration: 1,
+  delay: 1.3,
+  scale: 1,
+  y: 85,
+});
+tl.to(".Angular", {
+  x: 140,
+  y: 150,
+  duration: 1,
+  delay: 1.5,
+});
+
+tl.to("#circlereact", {
+  duration: 1,
+  delay: 1,
+  scale: 1,
+  y: 85,
+});
+tl.to("#circlereact", {
+  x: -170,
+  y:-60,
+  duration: 1,
+  delay: 3,
+});
+tl.to(".react", {
+  duration: 1,
+  delay: 1.3,
+  scale: 1,
+  y: 85,
+});
+tl.to(".react", {
+  x: -170,
+  y: -60,
+  duration: 1,
+  delay: 1.5,
+});
+
+tl.to("#circlepython", {
+  duration: 1,
+  delay: 1,
+  scale: 1,
+  y: 85,
+});
+tl.to("#circlepython", {
+  x: 140,
+  y: 330,
+  duration: 1,
+  delay: 3,
+});
+tl.to(".python", {
+  duration: 1,
+  delay: 1.3,
+  scale: 1,
+  y: 85,
+});
+tl.to(".python", {
+ x: 135,
+  y:310,
+  duration: 1,
+  delay: 1.5,
+});
